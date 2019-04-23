dcd029b347928c2ede03c983f35ca74020162663
Roberto.Fernandez <rob21.94@hotmail.com>
2019-04-22 23:16:27 -0400

    Create editMedicamentoBox.js

---
+++ porta.l-backup/js/app/common/editMedicamentoBox.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    var $userForm = $("#editMedicamentoForm");
+    validator.init($userForm, function() {
+        app.ajaxPost($userForm, function(response) {
+            bootbox.alert({
+                message: response.message,
+                callback: function() {
+                    if(!_.isEmpty(response.redirect_url)) {
+                        location.href = response.redirect_url;
+                    }
+                }
+            });
+        }, function(xhr, response) {
+            //Add Validation Error server side
+        });
+    }, function() {
+        bootbox.alert("Debe corregir los errores antes de proceder");
+    });
+});
+
