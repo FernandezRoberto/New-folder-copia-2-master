1550bde75245a486862a9b016439ff8b8f9d3fd4
Roberto.Fernandez <rob21.94@hotmail.com>
2019-04-22 23:16:13 -0400

    Update __init__.py

--- porta.l-backup/controllers/__init__.py
+++ porta.l-backup/controllers/__init__.py
@@ -2,6 +2,7 @@ from Common import *
 from User import *
 from Paciente import *
 from Categoria import *
+from Medicamento import *
 from Conocimiento import *
 from HistoriaClinica import *
 from Inferencia import *
