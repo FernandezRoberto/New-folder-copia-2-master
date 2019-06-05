53652e7efbb53bbfbca3120ded55dd3a6db8397c
Roberto.Fernandez <rob21.94@hotmail.com>
2019-06-05 16:41:17 -0400

    Update agregarReceta.js

--- porta.l-backup/js/app/common/agregarReceta.js
+++ porta.l-backup/js/app/common/agregarReceta.js
@@ -1,10 +1,31 @@
 function agregarReceta() {
-            // get assign the values from each text input
-            var receta = document.getElementById('receta');
+    // get assign the values from each text input
+    var receta = document.getElementById('receta');
+    // assign multiple text values
+    var medicamento = $("#medicamento").val();
+    var resultado = receta.value+medicamento+"\n";
+    receta.value = resultado;
+}
 
-            // assign multiple text values
-            var medicamento = $("#medicamento").val();
+function agregarTratamientos() {
+	var valor = document.getElementById("tipoTratamiento");
+    var texto = valor.options[valor.selectedIndex].text;
+    var resultado = texto;
+    receta.value = resultado;
+}
 
-            var resultado = receta.value+medicamento+"\n";
-            receta.value = resultado;
-        }
+function getval(sel)
+{
+  if(sel.value==='Medicamentos')
+  {
+    $(".agregarTratamiento").hide();
+    $("#medica").show();
+    $("#imprimirRecetas").show();
+  }
+  else
+  {
+    $(".agregarTratamiento").show();
+    $("#medica").hide();
+    $("#imprimirRecetas").hide();
+  }
+}
